6bd1385d0768f86b19508220021901e308995b80
Hrishikesh Belgaumkar <hribgm2496@gmail.com>
2019-08-09 18:42:34 +0530

    Updates

---
+++ precache-manifest.9521ab7a1aa4b40bd165993d0ebfa621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812c8101d2f5bc8a816eec72082c81f0",
+    "url": "/moviex-react/index.html"
+  },
+  {
+    "revision": "7335407d844a9e9d5ee4",
+    "url": "/moviex-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7684408bd5554961719d",
+    "url": "/moviex-react/static/js/2.4f057907.chunk.js"
+  },
+  {
+    "revision": "7335407d844a9e9d5ee4",
+    "url": "/moviex-react/static/js/main.3be24661.chunk.js"
+  },
+  {
+    "revision": "de62260238f134e3faeb",
+    "url": "/moviex-react/static/js/runtime~main.24245c1f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/moviex-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
